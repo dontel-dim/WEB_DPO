5da28089fac4fa63b8257d29b93ed70f8081f49c
shvabriq <s.shibakova@mail.ru>
2025-05-07 22:25:58 +0500

    сделана js валидация, исправлена валидация django, убрано окно логин в форме

--- Gallery/paintingapp/forms.py
+++ Gallery/paintingapp/forms.py
@@ -24,8 +24,8 @@ class UserLoginForm(AuthenticationForm):
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label='Ваше имя')
-    email = forms.EmailField(required=True, label='Ваш Email')
+    name = forms.CharField(max_length=100, required=True, label='Ваше имя', widget=forms.TextInput(attrs={'placeholder':'Введите ваше имя'}))
+    email = forms.EmailField(required=True, label='Ваш адрес электронной почты', widget=forms.EmailInput(attrs={'placeholder':'example@example.com'}))
     message = forms.CharField(max_length=200, min_length=10, widget=forms.Textarea, required=True, label='Сообщение')
 
 class ArtworkApplicationForm(forms.Form):
@@ -33,7 +33,7 @@ class ArtworkApplicationForm(forms.Form):
     artwork_description = forms.CharField(max_length=200, min_length=10, widget=forms.Textarea, required=True, label='Описание картины')
     price = forms.DecimalField(decimal_places=2, max_digits=10, required=True, label='Цена (в рублях)')
     artwork_image = forms.ImageField(required=True, label='Изображение картины')
-    phone_number = forms.CharField(max_length=15, required=True, label='Номер телефона')
+    phone_number = forms.CharField(max_length=15, required=True, label='Номер телефона', widget=forms.TextInput(attrs={'placeholder':'+7XXXXXXXXXX'}))
     artist_name = forms.CharField(max_length=100, required=True, label='Имя художника')
     artist_age = forms.IntegerField(required=True, label='Возраст художника')
     artist_description = forms.CharField(max_length=200, min_length=10, widget=forms.Textarea, required=True, label='Описание художника')
